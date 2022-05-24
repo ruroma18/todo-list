3447b6e33bc164802e478beeca16cca10ce87e21
Roman Rudenko <ruroma18@gmail.com>
2022-05-24 15:25:21 +0300

    Delete clg

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -6,7 +6,6 @@ import reducer from "./reducer";
 import ACTION_TYPES from "./actionTypes";
 
 const Todo = () => {
-  // const [taskList, setTaskList] = useState([]);
 
   const [state, dispatch] = useReducer(reducer, {taskList: []})
   console.log(state)
@@ -24,7 +23,6 @@ const Todo = () => {
     formikBag.resetForm();
   };
 
-  console.log(state);
   return (
     <article className={styles.container}>
       <h1 className={styles.heading}>ToDo List</h1>
